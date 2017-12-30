c8601dd293a99bc2a6b3305f04c537f14b378ef7
Michael Economy <meconomy@goodreads.com>
2017-12-30 10:07:02 -0800

    change button to say OK instead

---
+++ GameRankr/ArrayExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  ArrayExtensions.swift
+//  GameRankr
+//
+//  Created by Michael on 12/30/17.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+import Foundation
--- GameRankr/Controllers/Shared/AlertAPIErrorDelegate.swift
+++ GameRankr/Controllers/Shared/AlertAPIErrorDelegate.swift
@@ -4,7 +4,7 @@ protocol AlertAPIErrorDelegate : APIErrorDelegate {}
 extension UIViewController {
     func easyAlert(_ error: String) {
         let alert = UIAlertController(title: "Alert", message: error, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 }
