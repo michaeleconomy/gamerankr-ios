49d378f8f5c1d79d9a5f784c2b85b1f92aaf64a6
Michael Economy <meconomy@goodreads.com>
2018-01-09 18:07:08 -0800

    about page

---
+++ GameRankr/Controllers/AboutViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var contactButton: UIButton!
+    @IBOutlet weak var stefaniaLabel: UILabel!
+    @IBOutlet weak var icons8Label: UILabel!
+    @IBOutlet weak var giantBombLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contactButton.addTarget(self, action: #selector(contactTouch), for: .touchUpInside)
+        stefaniaLabel.isUserInteractionEnabled = true
+        stefaniaLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(stefaniaTouched)))
+        icons8Label.isUserInteractionEnabled = true
+        icons8Label.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(icons8Touched)))
+        giantBombLabel.isUserInteractionEnabled = true
+        giantBombLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(giantBombTouched)))
+    }
+    
+    @objc func contactTouch() {
+        openUrl("https://www.gamerankr.com/contact")
+    }
+    
+    @objc func stefaniaTouched() {
+        openUrl("http://www.graficheria.it")
+    }
+    
+    @objc func icons8Touched() {
+        openUrl("https://www.icons8.com")
+    }
+    
+    @objc func giantBombTouched() {
+        openUrl("https://www.giantbomb.com")
+    }
+    
+    func openUrl(_ string: String) {
+        guard let url = URL(string: string) else {
+            NSLog("coudln't make a url out of: \(string)")
+            return
+        }
+        
+        UIApplication.shared.open(url) {
+            boolean in
+            if (!boolean) {
+                NSLog("could not open the url for some reason!")
+            }
+        }
+    }
+}
